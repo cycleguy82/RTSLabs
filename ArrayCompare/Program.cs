bcc719872706319bccf8ea98e4a5c978b5ccc957
Shaw-v <cshaw-v@ad.davisvision.com>
2020-11-19 08:45:04 -0500

    Initial Commit

---
+++ ArrayCompare/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArrayCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nums = string.Empty;
+            do
+            {
+                Console.Write("Enter a Comma Delimited List of Numbers: ");
+                nums = Console.ReadLine();
+           } while (!(nums.Any(char.IsDigit) && nums.Contains(",") && !string.IsNullOrEmpty(nums)));
+
+
+            int c;
+            do
+            {
+                Console.Write("Ender a Number: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out c));
+
+            List<string> splitnums = nums.Split(',').ToList();
+
+
+
+            splitnums.ForEach(delegate (string st)
+            {
+                int n;
+                if (int.TryParse(st, out n))
+                {
+                    if (n == c)
+                        Console.WriteLine(n + " is Equal to " + c);
+                    else
+                        Console.WriteLine(n + " is " + (c.CompareTo(n) == 1 ? "Below " : "Above ") + c);
+
+                }
+            });
+
+            Console.Write("Press ENTER to Close Application.");
+            Console.Read();
+        }
+    }
+}
---
+++ WordShift/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+namespace WordShift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var word = string.Empty;
+            while (string.IsNullOrEmpty(word))
+            {
+                Console.Write("Please Enter Word To Shift: ");
+                word = Console.ReadLine();
+            }
+            string num = string.Empty;
+
+            //because when the last time you actually used a do-while  :)
+            do
+            {
+                StringBuilder st = new StringBuilder();
+                st.Append("Number of Characters To Shift To The Right ");
+                if (word.Length == 0)
+                    st.Append("(Length of 1)");
+                else
+                    st.Append("(From 0 to " + (word.Length == 1 ? 1 : (word.Length - 1)) + ")");
+                st.Append(": ");
+                Console.Write(st.ToString());
+                num = Console.ReadLine();
+
+            } while (!ValidateNumber(word, num));
+
+            Console.WriteLine("The Word Shifted By " + num + " to the Right is : " + ShiftString(word, int.Parse(num)));
+
+            Console.WriteLine("Press ENTER to Close Application");
+            Console.Read();
+        }
+
+        private static bool ValidateNumber(string word, string num)
+        {
+            int n;
+            if (!int.TryParse(num, out n))
+                return false;
+            else if (string.IsNullOrEmpty(word))
+                return false;
+            //Doing 0 or Length returns same result as input
+            //Lots of validation here; handling case of 1 character put in
+            //in addition to standard range checking
+            else if (n < 0 || (n > (word.Length - 1) && !(word.Length == 1 && n == 1)))
+                return false;
+
+            return true;
+        }
+
+        public static string ShiftString(string t, int n)
+        {
+            return t.Substring(t.Length - n) + t.Substring(0, (t.Length - n));
+        }
+    }
+}
